50bbb5bd927f0b1c5aaf6dc932431d67fadae60d
brianserem <brianseremiat@gmail.com>
2022-08-28 21:55:28 +0300

    wrote wait for db command and update CI files

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -16,7 +16,6 @@ class CommandTest(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
 
-    
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
 
